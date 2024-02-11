10c6f081694d3ed83b75f37d3b1062710073806e
PlagueManRodi <114426282+PlagueManRodi@users.noreply.github.com>
2024-02-11 07:02:35 -0500

    Update type matchups to modern games

--- data/types/type_matchups.asm
+++ data/types/type_matchups.asm
@@ -66,6 +66,7 @@ TypeEffects:
 	db BUG,          FLYING,       NOT_VERY_EFFECTIVE
 	db BUG,          PSYCHIC_TYPE, SUPER_EFFECTIVE
 	db BUG,          GHOST,        NOT_VERY_EFFECTIVE
+	db BUG, 	 POISON,       NOT_VERY_EFFECTIVE
 	db ROCK,         FIRE,         SUPER_EFFECTIVE
 	db ROCK,         FIGHTING,     NOT_VERY_EFFECTIVE
 	db ROCK,         GROUND,       NOT_VERY_EFFECTIVE
