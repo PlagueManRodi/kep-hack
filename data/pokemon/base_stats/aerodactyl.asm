08ca24ff49a6cbe51ba6eb30d1676a4a4ddb4c24
PlagueManRodi <114426282+PlagueManRodi@users.noreply.github.com>
2024-02-14 17:52:37 -0500

    Update aerodactyl.asm

--- data/pokemon/base_stats/aerodactyl.asm
+++ data/pokemon/base_stats/aerodactyl.asm
@@ -15,9 +15,10 @@
 
 	; tm/hm learnset
 	tmhm RAZOR_WIND,   WHIRLWIND,    TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  \
-	     HYPER_BEAM,   RAGE,         DRAGON_RAGE,  MIMIC,        DOUBLE_TEAM,  \
-	     REFLECT,      BIDE,         FIRE_BLAST,   SWIFT,        SKY_ATTACK,   \
-	     REST,         SUBSTITUTE,   FLY,          STRENGTH
+	     HYPER_BEAM,   RAGE,         DRAGON_RAGE,  EARTHQUAKE,   MIMIC,        \
+	     DOUBLE_TEAM,  REFLECT,      BIDE,         FIRE_BLAST,   SWIFT,        \
+	     SKY_ATTACK,   REST,         ROCK_SLIDE,   SUBSTITUTE,   FLY,          \
+	     STRENGTH
 	; end
 
 	db BANK(AerodactylPicFront)
