dfe09f590c389b178d95dcae0c4200da15d89214
PlagueManRodi <114426282+PlagueManRodi@users.noreply.github.com>
2024-02-15 01:24:16 -0500

    Update nidoranm.asm

--- data/pokemon/base_stats/nidoranm.asm
+++ data/pokemon/base_stats/nidoranm.asm
@@ -17,7 +17,7 @@
 	tmhm TOXIC,        HORN_DRILL,   BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  \
 	     BLIZZARD,     RAGE,         THUNDERBOLT,  THUNDER,      MIMIC,        \
 	     DOUBLE_TEAM,  REFLECT,      BIDE,         SKULL_BASH,   REST,         \
-	     SUBSTITUTE,   CUT,          STRENGTH
+	     SUBSTITUTE,   CUT,          STRENGTH,     FLASH
 	; end
 
 	db BANK(NidoranMPicFront)
