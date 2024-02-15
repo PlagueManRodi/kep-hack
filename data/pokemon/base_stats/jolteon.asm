b522f7b037995d9408874f857cae038a64c2ac87
PlagueManRodi <114426282+PlagueManRodi@users.noreply.github.com>
2024-02-15 01:46:36 -0500

    Update jolteon.asm

--- data/pokemon/base_stats/jolteon.asm
+++ data/pokemon/base_stats/jolteon.asm
@@ -17,7 +17,7 @@
 	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  HYPER_BEAM,   \
 	     RAGE,         THUNDERBOLT,  THUNDER,      MIMIC,        DOUBLE_TEAM,  \
 	     REFLECT,      BIDE,         SWIFT,        SKULL_BASH,   REST,         \
-	     THUNDER_WAVE, SUBSTITUTE,   FLASH,        STRENGTH
+	     THUNDER_WAVE, SUBSTITUTE,   STRENGTH,     FLASH
 	; end
 
 	db BANK(JolteonPicFront)
